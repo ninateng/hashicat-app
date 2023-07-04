9687e950ece2003f6d4946f05a6ea9ee4819be65
ninateng <niinateng@gmail.com>
2023-07-04 00:42:13 +0000

    Added Backup Storage module

--- main.tf
+++ main.tf
@@ -201,10 +201,12 @@ resource "null_resource" "configure-cat-app" {
     }
 
   }
-  
-      module "backupstorage" {
-      source = "app.terraform.io/sample-costco/azure-backup/azurerm"
-      version = "1.0.2"
-      resource_group_name = "${var.prefix}-private"
-    }
+
+}
+
+
+module "backupstorage"{
+  source = "app.terraform.io/sample-costco/azure-backup/azurerm"
+  version = "1.0.2"
+  resource_group_name = "${var.prefix}-private"
 }
